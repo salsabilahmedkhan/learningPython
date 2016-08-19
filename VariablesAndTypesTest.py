fc4baf91ee0ef63d2f983c032c84b3a2b583437c
salsabilahmedkhan <salsabil.ak94@gmail.com>
2016-08-20 00:21:37 +0600

    VariablesAndTypesTest

---
+++ VariablesAndTypesTest.py
@@ -0,0 +1,29 @@
+myint = 7
+print (myint)
+
+myfloat = 7.0
+myfloat = float(7)
+print (myfloat)
+
+mystring = 'hello'
+mystring = "hello"
+print (mystring)
+
+mystring = "Don't worry about apostrophes"
+print (mystring)
+
+one = 1
+two = 2
+three = one + two
+print (three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print (helloworld)
+
+a, b = 3,4
+print(a,b)
+print(a+b)
+print (a)
+print(b)
