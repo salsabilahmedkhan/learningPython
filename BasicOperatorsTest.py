7cf2f513d4fac6ff1a7fcd91cef9e2ec4aa2d30b
salsabilahmedkhan <salsabil.ak94@gmail.com>
2016-08-20 00:20:38 +0600

    BasicOperatorsTest

---
+++ BasicOperatorsTest.py
@@ -0,0 +1,21 @@
+number = 1+2*3/4.0
+print(number)
+
+remainder = 11%3
+print(remainder)
+
+squared=7**2
+cubed=7**3
+
+print(squared)
+print(cubed)
+
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = even_numbers + odd_numbers
+
+print(all_numbers)
+print [1,2,3]*3
